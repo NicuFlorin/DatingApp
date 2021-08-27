878b7629ce8b746dae0964a85a95335d51cb32e0
NicuFlorin <nasturicanicolae99@gmail.com>
2021-08-27 17:50:55 +0300

    End of section 7

--- API/DTO/RegisterDto.cs
+++ API/DTO/RegisterDto.cs
@@ -1,13 +1,16 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace API.DTO{
+namespace API.DTO
+{
 
-   public class RegisterDto{
+    public class RegisterDto
+    {
 
-       [Required]
-       public string username { get; set; }
-       [Required]
-       public string password { get; set; }
-   }
- 
+        [Required]
+        public string username { get; set; }
+        [Required]
+        [StringLength(8, MinimumLength = 4)]
+        public string password { get; set; }
+    }
+
 }
